3df39697179fd310f25a91cc8f63a8403aee4beb
castlemin999 <castlemin999@gmail.com>
2023-01-18 16:25:22 +0900

    feat(20230118): use VueCLI

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+  <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터"></app-header> -->
+  <app-header 
+    v-bind:propsdata="str"
+    v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+// let AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
+export default {
+  data: function () {
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // e.preventDefault();
+      console.log(this.username, this.password);
+      let url = 'https://jsonplaceholder.typicode.com/users';
+      let data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(err){
+          console.log(err);
+        })
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
